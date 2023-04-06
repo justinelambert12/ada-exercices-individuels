1505847ac9ede36f9bce99a7b9aba7b713391434
Justine <justine-lambert@hotmail.fr>
2023-04-06 12:33:31 +0200

    Etape 2 réussie mais la logique fait qu'au-delà de sapin(7), l'arbre décroit. A corriger en ajoutant des étages pour chaque nouveau pattern ?

--- 08-Sapin/sapin.js
+++ 08-Sapin/sapin.js
@@ -18,3 +18,38 @@ function sapin_etape1(number){
 // console.log("sapin_etape1(5)", sapin_etape1(5))
 
 //ETAPE 2
+function sapin(number){
+    let tree = ["  +", " /*\\", "/***\\"]
+    if (number>1) {
+        for (let i=0; i<number-1; i++){
+            let newFloor = createNewFloor(tree);
+            tree=tree.map(e=>" "+e);
+            tree.push(newFloor);
+        }
+    }
+    return tree.join("\n")
+}
+
+function createNewFloor(tree){
+    let lastFloor = tree[tree.length-1]
+    let newFloor = "";
+    if (tree.length>=4 && (tree.length-4)%3==0){
+        let indexPattern = (tree.length-4)/3;
+        newFloor = beginNewPattern(lastFloor, indexPattern);
+    }
+    else {
+        newFloor = lastFloor.slice(0,-2)+"**"+lastFloor.slice(-2);
+    }
+    return newFloor;
+}
+
+function beginNewPattern(lastFloor, indexPattern){
+    let newFloor = " ".repeat(indexPattern+1)+lastFloor.slice(0, -2-indexPattern*2)+lastFloor.slice(-2);
+    return newFloor;
+}
+
+// console.log(sapin(1), "sapin(1)")
+// console.log(sapin(2), "sapin(2)")
+// console.log(sapin(5), "sapin(5)")
+console.log(sapin(7), "sapin(7)")
+console.log(sapin(15), "sapin(15)")
