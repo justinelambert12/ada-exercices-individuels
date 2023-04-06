2e827749d077a9ee68c96cbbe9a4bef31dd514f6
Justine <justine-lambert@hotmail.fr>
2023-04-06 14:24:00 +0200

    Renommage des fonctions de l'étape 2 pour repartir de 0 dans l'étape 3

--- 08-Sapin/sapin.js
+++ 08-Sapin/sapin.js
@@ -18,6 +18,43 @@ function sapin_etape1(number){
 // console.log("sapin_etape1(5)", sapin_etape1(5))
 
 //ETAPE 2
+function sapin_etape2(number){
+    let tree = ["  +", " /*\\", "/***\\"]
+    if (number>1) {
+        for (let i=0; i<number-1; i++){
+            let newFloor = createNewFloor_etape2(tree);
+            tree=tree.map(e=>" "+e);
+            tree.push(newFloor);
+        }
+    }
+    return tree.join("\n")
+}
+
+function createNewFloor_etape2(tree){
+    let lastFloor = tree[tree.length-1]
+    let newFloor = "";
+    if (tree.length>=4 && (tree.length-4)%3==0){
+        let indexPattern = (tree.length-4)/3;
+        newFloor = beginNewPattern_etape2(lastFloor, indexPattern);
+    }
+    else {
+        newFloor = lastFloor.slice(0,-2)+"**"+lastFloor.slice(-2);
+    }
+    return newFloor;
+}
+
+function beginNewPattern_etape2(lastFloor, indexPattern){
+    let newFloor = " ".repeat(indexPattern+1)+lastFloor.slice(0, -2-indexPattern*2)+lastFloor.slice(-2);
+    return newFloor;
+}
+
+// console.log(sapin_etape2(1), "sapin_etape2(1)")
+// console.log(sapin_etape2(2), "sapin_etape2(2)")
+// console.log(sapin_etape2(5), "sapin_etape2(5)")
+// console.log(sapin_etape2(7), "sapin_etape2(7)")
+// console.log(sapin_etape2(15), "sapin_etape2(15)")
+
+//ETAPE 3
 function sapin(number){
     let tree = ["  +", " /*\\", "/***\\"]
     if (number>1) {
@@ -47,9 +84,3 @@ function beginNewPattern(lastFloor, indexPattern){
     let newFloor = " ".repeat(indexPattern+1)+lastFloor.slice(0, -2-indexPattern*2)+lastFloor.slice(-2);
     return newFloor;
 }
-
-// console.log(sapin(1), "sapin(1)")
-// console.log(sapin(2), "sapin(2)")
-// console.log(sapin(5), "sapin(5)")
-console.log(sapin(7), "sapin(7)")
-console.log(sapin(15), "sapin(15)")
