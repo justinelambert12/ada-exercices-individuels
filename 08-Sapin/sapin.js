c706d6a59fb3d0f1bc522ea4ec3757d0dd0574fe
Justine <justine-lambert@hotmail.fr>
2023-04-06 15:59:32 +0200

    Simplification étape 1

--- 08-Sapin/sapin.js
+++ 08-Sapin/sapin.js
@@ -2,20 +2,20 @@
 
 //ETAPE 1
 function sapin_etape1(number){
-    let arrayFloors = ["", "  +", " /*\\", "/***\\"]
+    let tree = ["  +", " /*\\", "/***\\"]
     if (number>1) {
         for (let i=0; i<number-1; i++){
-            let lastFloor = arrayFloors[arrayFloors.length-1]
-            let newFloor = lastFloor.slice(0,2)+"**"+lastFloor.slice(2);
-            arrayFloors=arrayFloors.map(e=>" "+e);
-            arrayFloors.push(newFloor);
+            let lastFloor = tree[tree.length-1]
+            let newFloor = lastFloor.slice(0,-2)+"**"+lastFloor.slice(-2);
+            tree=tree.map(e=>" "+e);
+            tree.push(newFloor);
         }
     }
-    return arrayFloors.join("\n")
+    return tree.join("\n")
 }
-// console.log("sapin_etape1(1)", sapin_etape1(1))
-// console.log("sapin_etape1(2)", sapin_etape1(2))
-// console.log("sapin_etape1(5)", sapin_etape1(5))
+// console.log(sapin_etape1(1), "sapin_etape1(1)")
+// console.log(sapin_etape1(2), "sapin_etape1(2)")
+// console.log(sapin_etape1(5), "sapin_etape1(5)")
 
 //ETAPE 2
 function sapin_etape2(number){
@@ -106,7 +106,13 @@ function beginNewPattern(lastFloor, indexPattern){
     return newFloor;
 }
 
-console.log(sapin(1), "sapin(1)")
-console.log(sapin(2), "sapin(2)")
-console.log(sapin(5), "sapin(5)")
-console.log(sapin(7), "sapin(7)")
+// console.log(sapin(1), "sapin(1)")
+// console.log(sapin(2), "sapin(2)")
+// console.log(sapin(5), "sapin(5)")
+// console.log(sapin(7), "sapin(7)")
+
+//ETAPE 4
+function addDecoration(tree) {
+    const decorations = ["+", "o"]
+    
+}
