62696732460ac2b2753bcfad6586ba56ba1d9157
Justine <justine-lambert@hotmail.fr>
2023-04-19 15:39:48 +0200

    1ère étape validée, découpage d'une chaine de caractère avec un espace entre tous les caractères non identiques

--- 11-suite-de-conway/script.js
+++ 11-suite-de-conway/script.js
@@ -1,2 +1,19 @@
 // https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/11_suite_de_conway.md
 
+// ETAPE 1
+function decoupeChaine(chaine) {
+    let result="";
+    for (let char of chaine){
+        // console.log(char)
+        let last_char = result.slice(-1);
+        // console.log(last_char)
+        if (char!=last_char){
+            result+=" ";
+        }
+        result+=char;
+    }
+    return result;
+}
+
+console.log(decoupeChaine("ab"))
+console.log(decoupeChaine("aabbca"))
