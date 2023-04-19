39af2910d9a6a25de0233545481d4a5cd5deb5b1
Justine <justine-lambert@hotmail.fr>
2023-04-19 15:47:54 +0200

    Correction de l'étape 1 pour ne pas qu'il y ait d'espace au début de la chaine résultat

--- 11-suite-de-conway/script.js
+++ 11-suite-de-conway/script.js
@@ -5,7 +5,7 @@ function decoupeChaine(chaine) {
     let result="";
     for (let char of chaine){
         let last_char = result.slice(-1);
-        if (char!=last_char){
+        if (result.length>0 && char!=last_char){
             result+=" ";
         }
         result+=char;
@@ -13,7 +13,18 @@ function decoupeChaine(chaine) {
     return result;
 }
 
-// console.log(decoupeChaine("ab"))
-// console.log(decoupeChaine("aabbca"))
+console.log(decoupeChaine("ab"))
+console.log(decoupeChaine("aabbca"))
 
 // ETAPE 2
+// function decritChaine(chaine) {
+//     let result="";
+//     let current_char="";
+//     for (let char of chaine){
+//         let last_char = result.slice(-1);
+//         if ()
+//     }
+// }
+
+// console.log(decritChaine("ab"))
+// console.log(decritChaine("aabbca"))
