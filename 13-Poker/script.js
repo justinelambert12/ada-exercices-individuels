13650897140c28bf0b967fbd622ae54f16229ac5
Justine <justine-lambert@hotmail.fr>
2023-05-11 19:01:35 +0200

    Jeu du poker, etapes 1 et 2 reussies

---
+++ 13-Poker/script.js
@@ -0,0 +1,51 @@
+// https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/13_poker.md
+
+// ETAPE 1 : generation du deck
+function shuffle(array) {
+    // Je fais une copie du tableau en entrée pour ne pas le modifier directement
+    let remainingArr = [...array];
+    let shuffledArr = [];
+    while (remainingArr.length > 0) {
+        let randomIndex = Math.floor(Math.random() * remainingArr.length)
+        // J'extrais un élément au hasard du tableau "remainingArr" que je vide au fur et à mesure
+        let randomElement = remainingArr.splice(randomIndex, 1);
+        // Je le mets dans mon tableau de sortie
+        shuffledArr.push(randomElement[0]);
+    }
+    // console.log(shuffledArr);
+    return shuffledArr;
+}
+
+function createDeck() {
+    let deck = [];
+    ["♠︎", "♣︎","♡", "♢"].forEach(symbol => {
+        for (let i=1; i<=10; i++) {
+            deck.push(i+symbol);
+        }
+        ["J", "Q", "K"].forEach(head => {
+            deck.push(head+symbol);
+        })
+    })
+    return shuffle(deck);
+}
+
+// console.log(createDeck());
+
+// ETAPE 2 : Distribution de cartes
+function deal(numberOfCards) {
+    let playerHand = []
+    if (numberOfCards < deck.length) {
+        for (let i=0; i<numberOfCards; i++){
+            // Retire la premiere carte du paquet et la met dans la main du joueur
+            playerHand.push(deck.shift());
+        }
+    } else {
+        console.log("Pas assez de cartes dans le paquet.")
+    }
+    return playerHand;
+}
+
+let deck = createDeck();
+const player1 = deal(2);
+const player2 = deal(2);
+console.log(player1, player2, deck)
