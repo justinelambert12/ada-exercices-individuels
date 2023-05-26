0500bdd60b8b820ff48c3a50d37b08ed36564df7
Justine <justine-lambert@hotmail.fr>
2023-05-26 10:19:18 +0200

    Function addToDisplay: manage the display of .,+,-,*,/ when there is just 0 in the display

--- 12-Calculatrice/calculator.js
+++ 12-Calculatrice/calculator.js
@@ -3,13 +3,11 @@
 function addToDisplay() {
     const displayedElement = document.getElementById("display");
     let displayedText = displayedElement.innerText;
-    // Si l'affichage est à "0", on veut que la valeur de la première touche pressée apparaisse sans le "0", donc je vide "displayedText"
-    if (displayedText == "0") {
+    // Si l'affichage est à "0" et qu'on appuie sur une touche avec un chiffre, la valeur apparaît sans le "0", donc je vide "displayedText"
+    if (displayedText == "0" && !([".", "+", "-", "*", "/"].includes(event.target.value))) {
         displayedText = "";
     }
-
-    // il reste à gérer de ne pas afficher le "+" ou toutes autres opérations s'il n'y a pas encore de chiffres
-    // et de ne pas pouvoir mettre plusieurs points (séparation décimales) à la suite
+    // il faut gérer de ne pas pouvoir mettre plusieurs points (séparation décimales) à la suite
     displayedElement.innerText = displayedText + event.target.value;
 }
 
