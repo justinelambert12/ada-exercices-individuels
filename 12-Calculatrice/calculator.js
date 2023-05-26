d779041fa1a798304e766c365a60d2a3efc64cef
Justine <justine-lambert@hotmail.fr>
2023-05-26 10:55:14 +0200

    New function calculate() when key = is pressed. Calculate and display the result. Work only for the addition for the moment.

--- 12-Calculatrice/calculator.js
+++ 12-Calculatrice/calculator.js
@@ -1,7 +1,7 @@
 // https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/12_calculatrice_graphique_%F0%9F%A7%AE.md
+const displayedElement = document.getElementById("display");
 
 function addToDisplay() {
-    const displayedElement = document.getElementById("display");
     let displayedText = displayedElement.innerText;
     // Si l'affichage est à "0" et qu'on appuie sur une touche avec un chiffre, la valeur apparaît sans le "0", donc je vide "displayedText"
     if (displayedText == "0" && !([".", "+", "-", "*", "/"].includes(event.target.value))) {
@@ -12,3 +12,21 @@ function addToDisplay() {
 }
 
 // Il faut une fonction qui est appelée en appuyant sur égal pour faire le calcul
+function calculate() {
+    let displayedText = displayedElement.innerText;
+    let termsOfAddition = displayedText.split("+");
+    // je cherche le nombre max de décimales dans les termes de l'opération pour afficher le résultat avec le bon nombre de chiffres
+    let maxNumberDecimals = 0;
+    termsOfAddition.forEach(e => {
+        if (e.includes(".")) {
+            let numberDecimals = e.split(".")[1].length;
+            if (numberDecimals > maxNumberDecimals) {
+                maxNumberDecimals = numberDecimals;
+            }
+        } 
+    })
+    let floatsOfAddition = termsOfAddition.map(e => parseFloat(e));
+    let result = floatsOfAddition.reduce((accumulator, currentValue) => accumulator + currentValue);
+    // J'affiche le résultat avec le bon nombre de décimales
+    displayedElement.innerText = result.toFixed(maxNumberDecimals);
+}
