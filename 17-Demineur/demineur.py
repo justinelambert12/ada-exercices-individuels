6e18f28ac328f1c8578c33b9ae809c9541ff71a7
Justine <justine-lambert@hotmail.fr>
2023-06-09 12:18:36 +0200

    Functions to generate and display a grid filled with 0.

--- 17-Demineur/demineur.py
+++ 17-Demineur/demineur.py
@@ -1,2 +1,24 @@
 # https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/17_demineur.md
 
+def generate_grid(M, N, K):
+    grid = []
+    for i in range(M):
+        row = []
+        for j in range(N):
+            row.append("0")
+        grid.append(row)
+    return grid
+
+
+def display_grid(grid):
+    # width = len(grid[0])
+    height = len(grid)
+    for i in range(height):
+        row = grid[i]
+        for el in row:
+            print(el,end=" ")
+        print("\n")
+
+
+playGrid = generate_grid(3, 5, 0)
+display_grid(playGrid)
