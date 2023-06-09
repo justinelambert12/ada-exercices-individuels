b5ca7e2137793ab1481600e92330950db292c36b
Justine <justine-lambert@hotmail.fr>
2023-06-09 16:54:01 +0200

    Modify generate_grid to add filled cases randomly.

--- 17-Demineur/demineur.py
+++ 17-Demineur/demineur.py
@@ -1,24 +1,32 @@
 # https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/17_demineur.md
+import random
 
 def generate_grid(M, N, K):
+    #Grille vide
     grid = []
     for i in range(M):
         row = []
         for j in range(N):
             row.append("0")
         grid.append(row)
+    #K cases remplies aléatoirement
+    n_filled_cases = 0
+    while n_filled_cases < K:
+        x = random.randrange(M)
+        y = random.randrange(N)
+        if (grid[x][y] == "0"):
+            grid[x][y] = "X"
+            n_filled_cases+=1
+
     return grid
 
 
 def display_grid(grid):
-    # width = len(grid[0])
-    height = len(grid)
-    for i in range(height):
-        row = grid[i]
+    for row in grid:
         for el in row:
             print(el,end=" ")
         print("\n")
 
 
-playGrid = generate_grid(3, 5, 0)
+playGrid = generate_grid(3, 5, 2)
 display_grid(playGrid)
