54aaea8349c0a9885a62995e6b7ac91482df9d60
Justine <justine-lambert@hotmail.fr>
2023-06-13 11:28:12 +0200

    Refactoring of code to generate all the hidden grid from the beginning.

--- 17-Demineur/demineur.py
+++ 17-Demineur/demineur.py
@@ -1,26 +1,17 @@
 # https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/17_demineur.md
-# TODO : Refactor code -> change generation of play_grid by adding the number of bombs around each square which is not a bomb ?
+# TODO : Refactor code 
 # TODO : add user interface to click on square instead of selecting them in the terminal
 # TODO : when click on an empty square (0 bomb around) reveal all empty squares around ?   
+import copy
 import random
 
-def generate_play_grid(nb_row, nb_column, nb_bomb):
-    #Grille vide
+def generate_empty_grid(nb_row, nb_columns):
     grid = []
     for i in range(nb_row):
         row = []
-        for j in range(nb_column):
-            row.append("0")
+        for j in range(nb_columns):
+            row.append("?")
         grid.append(row)
-    #Cases "bombes" remplies aléatoirement
-    nb_filled_cases = 0
-    while nb_filled_cases < nb_bomb:
-        x = random.randrange(nb_column)
-        y = random.randrange(nb_row)
-        if (grid[y][x] == "0"):
-            grid[y][x] = "X"
-            nb_filled_cases+=1
-
     return grid
 
 
@@ -44,31 +35,12 @@ def display_grid(grid):
         j+=1
 
 
-def generate_displayed_grid(nb_row, nb_columns):
-    grid = []
-    for i in range(nb_row):
-        row = []
-        for j in range(nb_columns):
-            row.append("?")
-        grid.append(row)
-    return grid
-
-
 def select_square(x, y, grid):
     if (x<len(grid[0]) and y<len(grid)):
         return grid[y][x]
     else:
         print("Coordonnées en-dehors de la grille !")
         return ""
-
-
-def has_won(play_grid, displayed_grid):
-    #Je vérifie que les seules cases non découvertes ("?") qui restent sont celles qui ont des bombes ("X")
-    for i in range(len(play_grid)):
-        for j in range(len(play_grid[0])):
-            if (displayed_grid[i][j] == "?" and play_grid[i][j] != "X"):
-                return False
-    return True
 
 
 def has_bomb(x, y, grid):
@@ -88,18 +60,45 @@ def nb_bombs_around(x, y, grid):
     return nb_bombs
 
 
+def generate_play_grid(empty_grid, nb_bomb):
+    play_grid = copy.deepcopy(empty_grid)
+    #Cases "bombes" remplies aléatoirement
+    nb_filled_cases = 0
+    while nb_filled_cases < nb_bomb:
+        x = random.randrange(len(play_grid[0]))
+        y = random.randrange(len(play_grid))
+        if (play_grid[y][x] != "X"):
+            play_grid[y][x] = "X"
+            nb_filled_cases+=1
+    #Autres cases remplies par le compte de bombes autour (8 cases adjacentes)
+    for i in range(len(play_grid)):
+        for j in range(len(play_grid[0])):
+            if (play_grid[i][j] == "?"):
+                play_grid[i][j] = nb_bombs_around(j, i, play_grid)
+    return play_grid
+
+
+def has_won(play_grid, displayed_grid):
+    #Je vérifie que les seules cases non découvertes ("?") qui restent sont celles qui ont des bombes ("X")
+    for i in range(len(play_grid)):
+        for j in range(len(play_grid[0])):
+            if (displayed_grid[i][j] == "?" and play_grid[i][j] != "X"):
+                return False
+    return True
+
+
 def play():
     nb_row = 3
     nb_column = 5
     nb_bomb = 2
 
     game_on = True
-    play_grid = generate_play_grid(nb_row, nb_column, nb_bomb)
-    displayed_grid = generate_displayed_grid(nb_row, nb_column)
+    displayed_grid = generate_empty_grid(nb_row, nb_column)
+    play_grid = generate_play_grid(displayed_grid, nb_bomb)
     #Tours de jeu
     while (game_on):
         display_grid(displayed_grid)
-        display_grid(play_grid)
+        # display_grid(play_grid)
 
         selected_square = ""
         x = 0
@@ -114,10 +113,7 @@ def play():
             else:
                 selected_square = select_square(x, y, play_grid)
         #Révelation de la case
-        if (selected_square == "X"):
-            displayed_grid[y][x] = selected_square
-        else:
-            displayed_grid[y][x] = str(nb_bombs_around(x, y, play_grid))
+        displayed_grid[y][x] = selected_square
         print("\n")
         #Vérification des conditions de victoire ou défaite
         if (selected_square == "X"):
