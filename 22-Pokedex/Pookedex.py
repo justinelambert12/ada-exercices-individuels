61f42f7f005fc002fdc73ef06ab05d309c9e17d7
justinelambert12 <justine-lambert@hotmail.fr>
2023-07-20 11:35:51 +0200

    Stocker les données du fichier json dans une variable data

--- 22-Pokedex/Pookedex.py
+++ 22-Pokedex/Pookedex.py
@@ -1,1 +1,6 @@
 # https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/22_Pokedex.md
+import json;
+f = open("pokemon.json")
+# print(f)
+data = json.load(f)
+print(data)
