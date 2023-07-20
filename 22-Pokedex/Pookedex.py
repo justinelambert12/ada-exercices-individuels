cea002ab049fa207bee4014eb2d43c090a27b55d
justinelambert12 <justine-lambert@hotmail.fr>
2023-07-20 14:34:29 +0200

    Ajouter une fonction pour trier un tableau de pokemons par poids. Modifier la fonction getPokemonWeight() pour prendre en compte des poids donnés en float directement

--- 22-Pokedex/Pookedex.py
+++ 22-Pokedex/Pookedex.py
@@ -1,5 +1,5 @@
 # https://github.com/adatechschool/Exercices-individuels-Doria-Shafik-Paris/blob/master/22_Pokedex.md
-import json;
+import json
 f = open("pokemon.json")
 # print(f)
 data = json.load(f)
@@ -22,28 +22,49 @@ def getPokemonName(pokemon):
 # print(getPokemonName(pokemonTest))
 
 def getPokemonWeight(pokemon):
-    weightString = pokemon["weight"]
-    weightValue = float(weightString.split()[0])
+    weightInput = pokemon["weight"]
+    if type(weightInput) is not float:
+        weightValue = float(weightInput.split()[0])
+    else:
+        weightValue=weightInput
     return weightValue
 # print(getPokemonWeight(pokemonTest))
 # print(type(getPokemonWeight(pokemonTest)))
 
 def isPokemonHeavierThan(pokemon, minWeight):
     weight = getPokemonWeight(pokemon)
-    return weight>minWeight
+    return weight > minWeight
 # print(isPokemonHeavierThan(pokemonTest, 6.8))
 
 def getAllPokemonsHeavierThan(pokemons, minWeight):
     heavyPokemons = []
     for pokemon in pokemons:
-        if isPokemonHeavierThan(pokemon, minWeight): 
-            heavyPokemons.append({"name": getPokemonName(pokemon), "weight": getPokemonWeight(pokemon)})
-    if len(heavyPokemons)>0:
-        print(heavyPokemons)
-        print(f"Il y a {len(heavyPokemons)} pokemons de plus de {minWeight} kg.")
+        if isPokemonHeavierThan(pokemon, minWeight):
+            heavyPokemons.append({"name": getPokemonName(
+                pokemon), "weight": getPokemonWeight(pokemon)})
+    if len(heavyPokemons) > 0:
+        # print(heavyPokemons)
+        print(
+            f"Il y a {len(heavyPokemons)} pokemons de plus de {minWeight} kg.")
     else:
         print(f"Aucun pokemon ne pese plus de {minWeight} kg !")
     return heavyPokemons
-# getAllPokemonsHeavierThan(pokemonTable, 10)
+# getAllPokemonsHeavierThan(pokemonTable, 10) (modifié)
 
+def isPokemonOneHeavierPokemonstwo(pokemonOne, pokemonTwo):
+    return getPokemonWeight(pokemonOne)>getPokemonWeight(pokemonTwo)
 
+def sortPokemonByWeight(pokemons):
+    while True:
+        isSwapped = False
+        for j in range(len(pokemons)-1):
+            if isPokemonOneHeavierPokemonstwo(pokemons[j],pokemons[j+1]):
+                temp = pokemons[j+1]
+                pokemons[j+1] = pokemons[j]
+                pokemons[j] = temp
+                isSwapped = True
+        if isSwapped == False:
+            break
+    return pokemons
+# pokemonsHeavierThan100= getAllPokemonsHeavierThan(pokemonTable, 100)
+# print(sortPokemonByWeight(pokemonsHeavierThan100))
