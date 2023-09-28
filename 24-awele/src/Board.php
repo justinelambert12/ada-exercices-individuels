5046d0dd4e07b1445beb9392feeffa32c00452f2
justinelambert12 <justine-lambert@hotmail.fr>
2023-09-28 16:38:59 +0200

    [test] Test display of empty board (0 seed in each pit)

--- 24-awele/src/Board.php
+++ 24-awele/src/Board.php
@@ -1,14 +1,20 @@
 <?php declare(strict_types=1);
 final class Board
 {
+  private int $seedsInEachPit;
+  public function __construct(int $seedsInEachPit=4)
+  {
+    $this->seedsInEachPit = $seedsInEachPit;
+  }
+
   public function display(): string
   {
-    $expectedDisplay = " A "." B "." C "." D "." E "." F "."\n"
-      .sprintf('%1$s%1$s%1$s%1$s%1$s%1$s', '(4)')."\n"
-      .sprintf('%1$s%1$s%1$s%1$s%1$s%1$s', '(4)')."\n"
+    $display = " A "." B "." C "." D "." E "." F "."\n"
+      .sprintf('%1$s%1$s%1$s%1$s%1$s%1$s', '('.$this->seedsInEachPit.')')."\n"
+      .sprintf('%1$s%1$s%1$s%1$s%1$s%1$s', '('.$this->seedsInEachPit.')')."\n"
       ." G "." H "." I "." J "." K "." L ";
     
-    print($expectedDisplay);
-    return $expectedDisplay;
+    print($display);
+    return $display;
   }
 }
--- 24-awele/tests/BoardTest.php
+++ 24-awele/tests/BoardTest.php
@@ -14,4 +14,15 @@ final class BoardTest extends TestCase
     
     $this->assertSame($expectedDisplay, $beginningBoard->display());
   }
+
+  public function testDisplayEmptyBoard(): void
+  {
+    $emptyBoard = new Board(0);
+    $expectedDisplay = " A "." B "." C "." D "." E "." F "."\n"
+    .sprintf('%1$s%1$s%1$s%1$s%1$s%1$s', '(0)')."\n"
+    .sprintf('%1$s%1$s%1$s%1$s%1$s%1$s', '(0)')."\n"
+    ." G "." H "." I "." J "." K "." L ";
+
+    $this->assertSame($expectedDisplay, $emptyBoard->display());
+  }
 }
